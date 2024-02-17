646048d6c0fd262d032029faba7dd720327caac3
Josh <110232756+Dat-Pudding@users.noreply.github.com>
2024-02-17 04:37:34 +0100

    Crouching mechanic added
    - can Crouch & stand up
    - can't stand up when env is blocking
    - halved moveSpeed while Crouching

--- Dad - A journey/Assets/Scripts/PlayerMovement_SideScroll.cs
+++ Dad - A journey/Assets/Scripts/PlayerMovement_SideScroll.cs
@@ -3,33 +3,39 @@ using UnityEngine;
 
 public class PlayerMovement_SideScroll : MonoBehaviour
 {
-    Rigidbody2D playerRigidbody;
-    Transform playerTransform;
-    RaycastHit2D hit2D;
-
-    public Transform playerHead;
-    public Transform playerFeet;
+    public Transform playerHead; // Empty child GameObject at the top of the standing sprite
+    public Transform playerFeet; // Empty child GameObject at the top of the crouching sprite
+    public GameObject playerStanding; // Child GameObject containing the standing sprite
+    public GameObject playerCrouching; // Child GameObject containing the crouching sprite
 
     [SerializeField] float movementSpeed = 1.0f;
     [SerializeField] float jumpForce = 5.0f;
     [SerializeField] float playerMass = 1f;
     [SerializeField] float gravity = 9.81f;
 
+    Rigidbody2D playerRigidbody; // Rigidbody2D attached to the "Player" GameObject
+    Transform playerTransform;  // Transform of "Player" container GameObject
+    RaycastHit2D headRayHit2D;
+    RaycastHit2D footRayHit2D;
+    float horizontalInput;
     float xVelocity;
     float yVelocity;
     bool isOnGround;
-    float horizontalInput;
     bool isJumping;
+    public bool isCrouching;
+    public bool isStanding;
+    public bool canStandUp;
 
     void Start()
     {
         playerTransform = GetComponent<Transform>();
         playerRigidbody = GetComponent<Rigidbody2D>();
+
         playerRigidbody.gravityScale = gravity;
         playerRigidbody.mass = playerMass;
     }
 
-    void Update()
+    void Update() // Checking for Inputs, changing values accordingly
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
 
@@ -41,38 +47,67 @@ public class PlayerMovement_SideScroll : MonoBehaviour
         {
             isJumping = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+            isStanding = false;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching = false;
+            isStanding = true;
+        }
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() // Using Input bools to determine correct behaviour and apply accordingly
     {
         MoveLateral(playerTransform, movementSpeed, horizontalInput);
-        CheckForGround();
-        Debug.Log(isOnGround);
+        CheckForGround(playerTransform, playerFeet);
+        CheckForCeiling(playerTransform, playerHead);
+
         if (isOnGround && isJumping)
         {
             Jump(playerRigidbody, jumpForce);
         }
+
+        if (!isStanding && isCrouching)
+        {
+            GetCrouched(playerRigidbody);
+        }
+        if (!isCrouching && canStandUp)
+        {
+            StandUp(playerRigidbody, playerTransform);
+        }
+        DebugSignals();
     }
 
-    void MoveLateral(Transform player, float movementSpeed, float xAxisInput)
+    void MoveLateral(Transform player, float movementSpeed, float xAxisInput) // Takes overloads to perform lateral movement of the player's Transform
     {
-        xVelocity = xAxisInput * movementSpeed * Time.deltaTime;
-        player.Translate(new Vector2(xVelocity, 0), Space.World);
+        if (isStanding && canStandUp)
+        {
+            xVelocity = xAxisInput * movementSpeed * Time.deltaTime;
+            player.Translate(new Vector2(xVelocity, 0), Space.World);
+        }
+        else
+        {
+            xVelocity = xAxisInput * (movementSpeed * 0.5f) * Time.deltaTime;
+            player.Translate(new Vector2(xVelocity, 0), Space.World);
+        }
     }
 
-    void Jump(Rigidbody2D player, float jumpForce)
+    void Jump(Rigidbody2D player, float jumpForce) // Applies an Impulse of value jumpForce to the player's Rigidbody2D
     {
         yVelocity = jumpForce * Time.deltaTime;
         player.AddForce(new Vector2(0, yVelocity), ForceMode2D.Impulse);
         isJumping = false;
     }
 
-    void CheckForGround()
+    void CheckForGround(Transform pTransform, Transform feet) // Casts Ray2D downwards from "Feet" Transform, sets bool true if hitting collider of tag "Ground"
     {
-        hit2D = Physics2D.Raycast(playerFeet.position, -playerTransform.up, 0.1f, 3);
-        Debug.DrawRay(playerFeet.position, -playerTransform.up, Color.red, 1f, false);
-        Debug.Log(hit2D.collider.name.ToString());
-        if (hit2D.collider.CompareTag("Ground"))
+        footRayHit2D = Physics2D.Raycast(feet.position, -pTransform.up, 0.1f, 3);
+
+        if (footRayHit2D.collider.CompareTag("Ground"))
         {
             isOnGround = true;
         }
@@ -80,5 +115,44 @@ public class PlayerMovement_SideScroll : MonoBehaviour
         {
             isOnGround = false;
         }
+    }
+
+    void GetCrouched(Rigidbody2D player) // Disables BoxCollider2D and "standing" GameObject
+    {
+        if (isCrouching)
+        {
+            playerStanding.SetActive(false);
+            player.GetComponent<BoxCollider2D>().enabled = false;
+            playerCrouching.SetActive(true);
+        }
+    }
+
+    void StandUp(Rigidbody2D player, Transform pTransform) // Enables BoxCollider2D and "standing" GameObject
+    {
+        if (canStandUp && isStanding)
+        {
+            playerStanding.SetActive(true);
+            player.GetComponent<BoxCollider2D>().enabled = true;
+            playerCrouching.SetActive(false);
+        }
+    }
+
+    void CheckForCeiling(Transform pTransform, Transform head) // Casts Ray2D upwards from "Head" Transform, sets bool true if hitting any collider except "Player"
+    {
+        headRayHit2D = Physics2D.Raycast(head.position, pTransform.up, 0.5f, 3);
+        if (headRayHit2D.collider == null)
+        {
+            canStandUp = true;
+        }
+        else
+        {
+            canStandUp = false;
+        }
+    }
+
+    void DebugSignals() // Casting RayLines as Gizmo for Editor
+    {
+        Debug.DrawRay(playerHead.position, playerTransform.up, Color.green, 0.5f, false);
+        Debug.DrawRay(playerFeet.position, -playerTransform.up, Color.red, 0.5f, false);
     }
 }
